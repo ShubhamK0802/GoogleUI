2364ad3b23d7a1566a916671ea3d88666a2ce8af
ShubhamK0802 <131487456+ShubhamK0802@users.noreply.github.com>
2023-05-11 01:38:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const form = document.querySelector('form');
-const searchInput = document.getElementById('searchInput');
+    const searchInput = document.querySelector("#search-input");
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const query = searchInput.value.trim();
-  if (query) {
-    const url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-    window.location.href = url;
-  }
+searchInput.addEventListener("keydown", function(event) {
+    if(event.code === "Enter") {
+        search();
+    }
 });
+
+function search() {
+    const input = searchInput.value;
+
+    window.location.href = "https://www.google.com/search?q=" + input + "&rlz=1C5CHFA_enNZ948NZ948&oq=" + input + "&aqs=chrome.0.69i59l2j46i175i199i433j46i199i291i433j46j0i433j0j69i60.875j0j9&sourceid=chrome&ie=UTF-8"
+}
